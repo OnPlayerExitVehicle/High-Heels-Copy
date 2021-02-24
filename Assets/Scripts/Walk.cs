d6347246d16f68c91326a8b95819d8a262fd34d0
Ali Nazif Koca <onplayerexitvehicle@outlook.com>
2021-02-25 01:52:55 +0300

    chore: balance animator speed

--- Assets/Scripts/Walk.cs
+++ Assets/Scripts/Walk.cs
@@ -7,17 +7,22 @@ public class Walk: MonoBehaviour
 {
     [SerializeField] private float speed;
     [SerializeField] private int legCount = 1;
+    [SerializeField] private Animator animator;
     private void Awake()
     {
+        animator = GetComponent<Animator>();
         legCount = GetComponent<Pickup>().LegCount;
+        //animator.speed = 0.5f;
     }
     private void Update()
     {
-        transform.position += transform.forward * (Time.deltaTime * speed * (legCount));
+        
+        transform.position += transform.forward * (Time.deltaTime * speed /** (legCount)*/);
     }
 
     public void OnLegCountChanged(int newLegCount)
     {
+        animator.speed *= 0.75f;
         legCount = newLegCount;
     }
 }
